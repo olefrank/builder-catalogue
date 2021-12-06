9fcc07b6069a3d1c3668c9feb5e1d73abed6f8f2
Ole Frank Jensen <oj@livahealth.com>
2021-12-06 20:41:15 +0100

    refactor users endpoint to return list of full users

--- src/api/useUsers.ts
+++ src/api/useUsers.ts
@@ -1,15 +1,15 @@
 import useSWR from "swr";
-import { User } from "../models/User";
+import { FullUser } from "../models/User";
 import { baseUrl } from "./constants";
 
 export type UseUsers = {
-  users: User[] | undefined;
+  users: FullUser[] | undefined;
   isLoading: boolean;
   error: boolean;
 };
 
 function useUsers(): UseUsers {
-  const { data, error } = useSWR<User[]>(`${baseUrl}/api/users`);
+  const { data, error } = useSWR<FullUser[]>(`${baseUrl}/api/users`);
 
   return {
     users: data,
